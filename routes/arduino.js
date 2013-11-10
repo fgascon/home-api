7a92ca01496209ec91ed2722584df19191e80e26
Frédéric Gascon <frederic.gascon@gmail.com>
2013-11-10 15:38:21 -0500

    arduino routes

--- routes/arduino.js
+++ routes/arduino.js
@@ -1,27 +1,36 @@
 var arduino = require('../lib/arduino');
 
-var pins = (function(){
-	var pins = [];
-	for(var i=1; i<=13; i++){
-		pins.push(i.toString());
-	}
-	for(var i=1; i<=6; i++){
-		pins.push('A'+i.toString());
-	}
-	return pins;
-}());
-
 module.exports = function(app){
 	
-	app.post('/arduino/:pin', function(req, res){
-		var pin = req.params.pin;
-		if(pins.indexOf(pin) < 0){
-			res.json({
-				success: false,
-				error: "Invalid pin " + pin
-			});
-			return;
-		}
-		arduino.setPin(pin, req.body.state);
+	/*app.get('/arduino/:pin', function(req, res){
+		arduino.setPin(req.params.pin, req.body.state, function(err, value){
+			if(err){
+				res.json({
+					success: false,
+					error: err
+				});
+			}else{
+				res.json({
+					success: true,
+					value: value
+				});
+			}
+		});
 	});
+	
+	app.post('/arduino/:pin', function(req, res){
+		arduino.setPin(req.params.pin, req.body.state, function(err, value){
+			if(err){
+				res.json({
+					success: false,
+					error: err
+				});
+			}else{
+				res.json({
+					success: true,
+					value: value
+				});
+			}
+		});
+	});*/
 };
